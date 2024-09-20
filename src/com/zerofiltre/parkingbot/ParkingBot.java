84b2c8a301cc86f3786f87974480c64d95a373a1
Boumedienne-dev <103183418+Boumedienne-dev@users.noreply.github.com>
2024-09-20 09:52:31 +0200

    La portée d'une variable

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,6 +1,12 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
+
+
+  static String completion = "des équipes du parking Zerofiltre";
+  static ParkingService parkingService = new ParkingService();
 
   /**
    * Ceci est la méthode Main
@@ -17,11 +23,15 @@ public class ParkingBot {
    * Cette méthode permet de dire Hello
    */
   private static void sayHello() {
-    String welcomeSentence = "Hello, recevez la bienvenue ";
+    String welcomeSentence = "Hello, recevez la bienvenue " + completion;
     System.out.println(welcomeSentence);
+    String parkService = parkingService.parking;
+    String washingService = parkingService.washing;
+
+    System.out.println("Nous proposons les services suivantes: "+parkService+ " ET "+washingService);
   }
 
   private static void sayBye() {
-    System.out.println("Recevez les aurevoirs");
+    System.out.println("Recevez les aurevoirs " + completion);
   }
 }
