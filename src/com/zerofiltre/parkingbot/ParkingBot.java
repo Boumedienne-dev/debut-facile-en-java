d0f992fc66ca2df7946132c56ac1468178097962
Boumedienne-dev <103183418+Boumedienne-dev@users.noreply.github.com>
2024-09-23 10:07:47 +0200

    La POO

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,5 +1,9 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.model.Ticket;
+import com.zerofiltre.parkingbot.model.Vehicule;
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
 
 
@@ -9,8 +13,15 @@ public class ParkingBot {
    * @param args : Tableau de données entrées lors du lancement de l'application
    */
   public static void main(String[] args) {
+    processVehicules();
   }
 
-
-
+  private static void processVehicules() {
+    Vehicule vehicule = new Vehicule();
+    vehicule.setRegistrationNumber("LS-324-PM");
+    vehicule.setCategory("CITADINE");
+    ParkingService parkingService = new ParkingService();
+    Ticket ticket = parkingService.processIncomingVehicle(vehicule);
+    System.out.println(ticket);
+  }
 }
